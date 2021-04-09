11f816ae9b8bf47a32dcd3f10a4ec23050ef934d
caxerx <webmaster@caxerx.com>
2021-04-09 12:48:26 +0800

    Update build file

--- src-ui/vue.config.js
+++ src-ui/vue.config.js
@@ -5,5 +5,5 @@ const VuetifyLoaderPlugin = require("vuetify-loader/lib/plugin");
 module.exports = {
   filenameHashing: false,
   productionSourceMap: false,
-  outputDir: '../build/resources/main/dist'
+  outputDir: '../src/main/resources/dist'
 };
